6a0cf3ca8060017adf113eef377ce36d793a6edc
Nate <ndsunc@gmail.com>
2018-09-13 10:09:48 -0700

    Added rescue_from ActiveRecord::RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    render json: { error: 'Invalid User ID' }, status: 400
+  end
 end
