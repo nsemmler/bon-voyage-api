3b8f6e9ce210c983476b9325b16520e9e4e2cfc0
Nate <ndsunc@gmail.com>
2018-09-13 12:43:35 -0700

    Create new User with strong params via private method user_params()

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,7 @@ class UsersController < ApplicationController
 
   # Create new User
   def create
-    user = User.new(email: params[:email], password: params[:password])
-    # user = User.new(user_params)
+    user = User.new(user_params)
     if user.save
       render json: user
     else
@@ -29,6 +28,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.permit(:email, :password)
   end
 end
