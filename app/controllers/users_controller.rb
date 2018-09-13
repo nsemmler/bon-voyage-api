06cbaa5a3f2a97776e92af0509ab6c9be12cc30a
Nate <ndsunc@gmail.com>
2018-09-13 10:09:25 -0700

    Updated UsersController to rename actions to index, show, create; All methods working with error handling

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  # Get all Users
+  def index
+    users = User.all
+    render json: users
+  end
+
+  # Get a single User
+  def show
+    if params[:id]
+      user = User.find(params[:id].to_i)
+      render json: user
+    else
+      render json: { error: 'Invalid User ID' }, status: 400
+    end
+  end
+
+  # Create new User
+  def create
+    user = User.new(email: params[:email], password: params[:password])
+
+    if user.save
+      render json: user
+    else
+      render json: { errors: user.errors.full_messages }, status: 400
+    end
+  end
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(email: "user@gmail.com", password: "password")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "      "
+    assert_not @user.valid?
+  end
+
+  test "password should be present" do
+    @user.password = "        "
+    assert_not @user.valid?
+  end
+end
