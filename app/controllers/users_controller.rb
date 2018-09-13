71694b630dbb830b6a22ef03be3b06500e9cd818
Nate <ndsunc@gmail.com>
2018-09-13 11:02:26 -0700

    Encrypting User passwords w/ bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,11 +18,17 @@ class UsersController < ApplicationController
   # Create new User
   def create
     user = User.new(email: params[:email], password: params[:password])
-
+    # user = User.new(user_params)
     if user.save
       render json: user
     else
       render json: { errors: user.errors.full_messages }, status: 400
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :email,
     presence: true,
     length: { minimum: 5, maximum: 255 }
+  has_secure_password
   validates :password,
     presence: true,
     length: { minimum: 6 }
