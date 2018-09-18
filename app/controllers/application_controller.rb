a68f4f3ddd986bb59fe510e1dece2b40edf26b56
Nate <ndsunc@gmail.com>
2018-09-18 12:35:50 -0700

    Added devise - users can log in and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::API
+  respond_to :json
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def render_resource (resource)
+    if resource.errors.empty?
+      render json: resource
+    else
+      validation_error(resource)
+    end
+  end
+
+  def validation_error (resource)
+    render json: {
+      errors: [
+        { status: '400', title: 'Bad Request', detail: resource.errors }
+      ]
+    }, status: :bad_request
+  end
 
   private
 
