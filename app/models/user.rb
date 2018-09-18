6508cd0eac6603ce7ab98da9ac5dc5057a02ab32
Nate <ndsunc@gmail.com>
2018-09-18 12:34:55 -0700

    Removed create(), login() actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise  :database_authenticatable,
+          :registerable,
+          :recoverable,
+          :rememberable,
+          :trackable,
+          :validatable
+
   validates_presence_of :email, :password
-  has_secure_password
+
   validates :email, length: { minimum: 5, maximum: 255 }
   validates :password, length: { minimum: 6 }
 end
