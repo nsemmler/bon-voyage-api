670edf26eb47be5ab84bd057911fcacf21ca659b
Nate <ndsunc@gmail.com>
2018-09-19 13:24:32 -0700

    Configuration for finding the local_env.yml file for generating ENV variables like the JWT secret key

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,13 @@ module BonVoyageApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'http://localhost:3000'
