4a625729aa46d63dd7764b70e3063283b38bfd0b
Nate <ndsunc@gmail.com>
2018-09-19 15:53:33 -0700

    Had to have at least one validation for password to have validates :password - kept length min at 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,5 @@ class User < ApplicationRecord
   validates_presence_of :email, :password
 
   validates :email, length: { minimum: 5, maximum: 255 }
-  validates :password
+  validates :password, length: { minimum: 6 }
 end
