2bc33b096ebb0c8a2bbf3e41dc2c176795d8b072
Nate <ndsunc@gmail.com>
2018-09-19 11:15:53 -0700

    Updated User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_many :trips, dependent: :destroy
+  validates_associated :trips
+
   validates_presence_of :email, :password
   has_secure_password
+  
   validates :email, length: { minimum: 5, maximum: 255 }
   validates :password, length: { minimum: 6 }
 end
