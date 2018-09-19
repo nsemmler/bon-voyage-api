0adee3ea63abf563f5e3a77b373a8a3e08d22cb8
Nate <ndsunc@gmail.com>
2018-09-19 15:22:35 -0700

    Updated configuration for User model and either removed redundant config settings or moved them into /config/initializers/devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :trips, dependent: :destroy
-  before_save { self.email = email.downcase }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -16,8 +15,6 @@ class User < ApplicationRecord
   validates_associated :trips
   validates_presence_of :email, :password
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  validates :email, length: { minimum: 5, maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  validates :password, length: { minimum: 6 }
+  validates :email, length: { minimum: 5, maximum: 255 }
+  validates :password
 end
