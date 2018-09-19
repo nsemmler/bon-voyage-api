62175dbe0c1db06013ae1d0eb1d4d2f9c48af05a
Nate <ndsunc@gmail.com>
2018-09-19 14:48:31 -0700

    Updated rake routes display at top of routes.rb with new routes for Tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,30 @@
 # ---------------------------------------------------------------------------------------------------------
-# Prefix Verb                URI Pattern                      Controller#Action
+# Prefix Verb                URI Pattern                        Controller#Action
 # ---------------------------------------------------------------------------------------------------------
-# new_user_session           GET    /login(.:format)          sessions#new {:format=>:json}
-# user_session               POST   /login(.:format)          sessions#create {:format=>:json}
-# destroy_user_session       DELETE /logout(.:format)         sessions#destroy {:format=>:json}
-# new_user_password          GET    /password/new(.:format)   devise/passwords#new {:format=>:json}
-# edit_user_password         GET    /password/edit(.:format)  devise/passwords#edit {:format=>:json}
-# user_password              PATCH  /password(.:format)       devise/passwords#update {:format=>:json}
-#                            PUT    /password(.:format)       devise/passwords#update {:format=>:json}
-#                            POST   /password(.:format)       devise/passwords#create {:format=>:json}
-# cancel_user_registration   GET    /signup/cancel(.:format)  devise/registrations#cancel {:format=>:json}
-# new_user_registration      GET    /signup/sign_up(.:format) devise/registrations#new {:format=>:json}
-# edit_user_registration     GET    /signup/edit(.:format)    devise/registrations#edit {:format=>:json}
-# user_registration          PATCH  /signup(.:format)         devise/registrations#update {:format=>:json}
-#                            PUT    /signup(.:format)         devise/registrations#update {:format=>:json}
-#                            DELETE /signup(.:format)         devise/registrations#destroy {:format=>:json}
-#                            POST   /signup(.:format)         devise/registrations#create {:format=>:json}
-# users                      GET    /users(.:format)          users#index
-#                            GET    /users/:id(.:format)      users#show
-#                            DELETE /users/:id(.:format)      users#destroy
-# root                       GET    /                         users#index
+# new_user_session           GET    /login(.:format)            sessions#new {:format=>:json}
+# user_session               POST   /login(.:format)            sessions#create {:format=>:json}
+# destroy_user_session       DELETE /logout(.:format)           sessions#destroy {:format=>:json}
+# new_user_password          GET    /password/new(.:format)     devise/passwords#new {:format=>:json}
+# edit_user_password         GET    /password/edit(.:format)    devise/passwords#edit {:format=>:json}
+# user_password              PATCH  /password(.:format)         devise/passwords#update {:format=>:json}
+#                            PUT    /password(.:format)         devise/passwords#update {:format=>:json}
+#                            POST   /password(.:format)         devise/passwords#create {:format=>:json}
+# cancel_user_registration   GET    /signup/cancel(.:format)    devise/registrations#cancel {:format=>:json}
+# new_user_registration      GET    /signup/sign_up(.:format)   devise/registrations#new {:format=>:json}
+# edit_user_registration     GET    /signup/edit(.:format)      devise/registrations#edit {:format=>:json}
+# user_registration          PATCH  /signup(.:format)           devise/registrations#update {:format=>:json}
+#                            PUT    /signup(.:format)           devise/registrations#update {:format=>:json}
+#                            DELETE /signup(.:format)           devise/registrations#destroy {:format=>:json}
+#                            POST   /signup(.:format)           devise/registrations#create {:format=>:json}
+# users                      GET    /users(.:format)            users#index
+#                            GET    /users/:id(.:format)        users#show
+#                            DELETE /users/:id(.:format)        users#destroy
+#                            GET    /users/:user_id/trips       trips#show
+#                            POST   /users/:user_id/trips       trips#create
+#                            PATCH  /users/:user_id/trips/:id   trips#update
+#                            DELETE /users/:user_id/trips/:id   trips#destroy
+#                            GET    /cc/:country_name           country_codes#getCCforCountryName
+# root                       GET    /                           users#index
 
 Rails.application.routes.draw do
   devise_for :users,
