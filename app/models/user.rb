3c1e4d420e5e6607a29bfb2f5c8df4bdf3337760
Nate <ndsunc@gmail.com>
2018-09-19 11:15:53 -0700

    Updated User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,11 @@ class User < ApplicationRecord
           :jwt_authenticatable,
           jwt_revocation_strategy: JWTBlacklist
 
+  has_many :trips, dependent: :destroy
+  validates_associated :trips
+
   validates_presence_of :email, :password
+  has_secure_password
 
   validates :email, length: { minimum: 5, maximum: 255 }
   validates :password, length: { minimum: 6 }
