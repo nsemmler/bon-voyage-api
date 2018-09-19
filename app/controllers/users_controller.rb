3661c9b69a09b3ca4e59c7e0ee2268c22de1b36c
Nate <ndsunc@gmail.com>
2018-09-19 13:25:18 -0700

    Updated User methods to return correct User data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,15 @@ class UsersController < ApplicationController
 
   # Get all Users
   def index
-    @users = User.all
-    render json: @users, status: 200, each_serializer: UserSerializer
+    users = User.all
+    render json: users, status: 200, each_serializer: UserSerializer
   end
 
   # Get a single User
   def show
     if params[:id]
-      @user = User.find(params[:id].to_i)
-      render json: @user
+      user = User.find(params[:id].to_i)
+      render json: user
     else
       render json: { error: 'Invalid User ID' }, status: 400
     end
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   # Delete User
   def destroy
     if params[:id]
-      @user = User.find(params[:id].to_i).destroy
+      user = User.find(params[:id].to_i).destroy
       render json: { success: "User destroyed" }, status: 204
     else
       render json: { error: 'Invalid User ID' }, status: 400
