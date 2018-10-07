cc9ba7375d12e4350cb5dba43afe5f09553198c7
Nate <ndsunc@gmail.com>
2018-10-07 13:06:54 -0700

    Updated model name to be singular - belongs_to :model is singular in Ruby

--- app/models/users_favorite.rb
+++ app/models/users_favorite.rb
@@ -1,6 +1,6 @@
 class UsersFavorite < ApplicationRecord
-  belongs_to :users, optional: true
-  belongs_to :countries, optional: true
+  belongs_to :user, optional: true
+  belongs_to :country, optional: true
 
   validates_presence_of :user_id, :country_id
 end
