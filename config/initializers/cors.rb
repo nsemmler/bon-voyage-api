a02ddbb24e41aa6fe495465f5a8ed9234e6e125d
Nate <ndsunc@gmail.com>
2018-10-09 14:24:08 -0700

    Removed API url, updated methods to accept :any

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,11 +9,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000'
     origins 'http://localhost:3001'
-    origins 'https://bon-voyage-api.herokuapp.com'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      methods: :any,
       expose: ['Authorization']
   end
 end
