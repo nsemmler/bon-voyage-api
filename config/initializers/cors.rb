54174317f84271672115484ffb74ea9bd8a151f9
Nate <ndsunc@gmail.com>
2018-10-10 09:11:30 -0700

    Removed conditional in POI creation rake that requires images

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://deafening-bird.surge.sh'
+    origins ['https://deafening-bird.surge.sh', 'locahost:3001']
 
     resource '*',
       headers: %w(Authorization),
