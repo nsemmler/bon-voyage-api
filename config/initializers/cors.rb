7643314c56945c8f3a714d2b4ba582f0ab4b008a
Nate <ndsunc@gmail.com>
2018-10-10 09:17:20 -0700

    Updated name of front end surge URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://deafening-bird.surge.sh', 'locahost:3001']
+    origins ['https://bonvoyage.surge.sh', 'locahost:3001']
 
     resource '*',
       headers: %w(Authorization),
